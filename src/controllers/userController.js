83418b2e74fee8700bc9be45e015e2e638e07c61
Billy <longnd1@bravebits.vn>
2023-11-13 11:00:31 +0700

    Fix name models

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
 const bcrypt = require("bcrypt");
