0f2d3620cc3e53d5b2c714a1732c7959d9cef480
Billy <longnd1@bravebits.vn>
2024-02-02 13:04:12 +0700

    Update socket

--- index.js
+++ index.js
@@ -41,15 +41,19 @@ const io = socketio(httpServer, {
 });
 
 let users = [];
-let comments = [];
 
 // Socket.io functions
-const handleUpdatePost = (post) => {
-  io.emit("getPost", post);
+const handleUpdatePost = (post, targetSocketId) => {
+  io.emit("getPost", { ...post, targetSocketId });
 };
 
-const handleUpdateComment = (post) => {
-  io.emit("getComment", post);
+const handleUpdateComment = (comment, targetSocketId) => {
+  io.emit("getComment", { ...comment, targetSocketId });
+};
+
+const handleChangeData = (data) => {
+  comments = data;
+  io.emit("getData", initData);
 };
 
 const handleAddUser = (userId, socketId) => {
@@ -70,15 +74,17 @@ const handleDisconnect = (socketId) => {
 // Socket.io event listeners
 io.on("connection", (socket) => {
   // Post
-  socket.on("updatePost", handleUpdatePost);
+  socket.on("updatePost", (post) => handleUpdatePost(post, socket.id));
 
   //Comment
-  socket.on("updateComment", handleUpdateComment);
+  socket.on("updateComment", (comment) =>
+    handleUpdateComment(comment, socket.id)
+  );
+  socket.on("changeData", handleChangeData);
   socket.on("addUser", (userId) => handleAddUser(userId, socket.id));
   socket.on("sendMessage", handleSendMessage);
   socket.on("disconnect", () => handleDisconnect(socket.id));
 });
-
 // Express middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
