a826d7cdb3f119f23a03791bb3f3bd2cd37c1bc4
Billy <longnd1@bravebits.vn>
2024-02-08 23:57:56 +0700

    Move socketio to other file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 // Import necessary modules
 const cors = require("cors");
 const express = require("express");
-const socketio = require("socket.io");
-const http = require("http");
 const cookieParser = require("cookie-parser");
 const connection = require("./src/config/database");
 const authRoute = require("./src/routes/auth");
@@ -14,6 +12,7 @@ const userRoute = require("./src/routes/user");
 const messageRoute = require("./src/routes/message");
 const conversationRoute = require("./src/routes/conversation");
 const { formattedGmt7Date } = require("./src/utils/utilities");
+const setupSocketIO = require("./src/socketManager");
 
 let timerCronjobSocket;
 let cronjobData = {};
@@ -33,62 +32,7 @@ const productionUrl = process.env.PRODUCTION_URL;
 const timeDelayCronjob = process.env.TIME_DELAY_CRONJOB;
 
 // Set up socket.io server
-const httpServer = http.createServer(app);
-const io = socketio(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
-
-// Object to store user information and their socketIds
-let users = {};
-
-const getOtherSocketIds = (currentSocketId) =>
-  Object.values(users).filter((item) => item !== currentSocketId);
-
-// Socket.io functions
-const handleUpdatePost = (post, targetSocketId) => {
-  const { userId } = post || {};
-  const currentSocketId = users[userId];
-
-  io.to(getOtherSocketIds(currentSocketId)).emit("getPost", {
-    ...post,
-    targetSocketId,
-  });
-};
-
-const handleUpdateComment = (comment, targetSocketId) => {
-  io.emit("getComment", { ...comment, targetSocketId });
-};
-
-const handleAddUser = (userId, socketId) => {
-  users[userId] = socketId;
-  io.emit("getUsers", users);
-};
-
-const handleDisconnect = (socketId) => {
-  // Remove user from the list upon disconnection
-  const disconnectedUserId = Object.keys(users).find(
-    (userId) => users[userId] === socketId
-  );
-  if (disconnectedUserId) {
-    delete users[disconnectedUserId];
-    io.emit("getUsers", users);
-  }
-};
-
-// Socket.io event listeners
-io.on("connection", (socket) => {
-  console.log("===>connection:", socket.id);
-
-  // Register events from client
-  socket.on("updatePost", (post) => handleUpdatePost(post, socket.id));
-  socket.on("updateComment", (comment) =>
-    handleUpdateComment(comment, socket.id)
-  );
-  socket.on("addUser", (userId) => handleAddUser(userId, socket.id));
-  socket.on("disconnect", () => handleDisconnect(socket.id));
-});
+const httpServer = setupSocketIO(app);
 
 // Express middleware
 app.use(express.json({ limit: "50mb" }));
